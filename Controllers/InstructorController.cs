1dd259fa2459e26277eea3e95b9b0bb7f6ad0642
Jawad-Hamdan03 <s12113703@stu.najah.edu>
2025-01-27 10:17:33 +0200

    All CRUD operations done for Instructor Controller

--- Controllers/InstructorController.cs
+++ Controllers/InstructorController.cs
@@ -9,9 +9,83 @@ namespace Assignment1.Controllers
         Assignment1DBContext DB = new Assignment1DBContext();
         public IActionResult Index()
         {
-           
             List<Instructor> InstructorsModel= DB.Instructors.Include(I=> I.Department).Include(i=> i.Course).ToList();
             return View("Index", InstructorsModel);
         }
+
+
+
+
+        public IActionResult Details(int id)
+        {
+            Instructor instructorModel= DB.Instructors.Include(i=>i.Course).Include(i=>i.Department).FirstOrDefault(i=> i.Id==id);
+            return View("Details",instructorModel);
+        }
+
+
+        public IActionResult AddInstructor()
+        {
+            ViewBag.Courses = DB.Courses.ToList();
+            return View("AddInstructor",DB.Departments.ToList());
+        }
+
+        public IActionResult SaveAdd(Instructor instructorReciever)
+        {
+            instructorReciever.ImageURL = "1.png";
+            DB.Instructors.Add(instructorReciever);
+            DB.SaveChanges();
+           return  RedirectToAction("Index");
+        }
+
+
+
+        public IActionResult Edit(int id, string department, string course)
+        {
+           Instructor instructorModel= DB.Instructors.FirstOrDefault(i => i.Id == id);
+            ViewBag.Departments= DB.Departments.ToList();
+            ViewBag.Courses= DB.Courses.ToList();
+            ViewBag.department=department;
+            ViewBag.course=course;
+            return View("Edit",instructorModel);
+        }
+
+        public IActionResult SaveEdit(Instructor instructorReciever) {
+
+            instructorReciever.ImageURL = "1.png";
+            
+            Instructor targetInstructor = DB.Instructors.FirstOrDefault(i=> i.Id == instructorReciever.Id);
+            if(targetInstructor != null)
+            {
+                targetInstructor.Id=instructorReciever.Id;
+                targetInstructor.Address=instructorReciever.Address;
+                targetInstructor.Salary=instructorReciever.Salary;
+                targetInstructor.CourseId=instructorReciever.CourseId;
+                targetInstructor.DepartmentId=instructorReciever.DepartmentId;
+                targetInstructor.Course=instructorReciever.Course;
+                targetInstructor.Department=instructorReciever.Department;
+
+                DB.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return Content("failed to edit");
+        }
+
+
+        public IActionResult Search(string Name)
+        {
+           List<Instructor> matchListModel= DB.Instructors.Include(i => i.Department).Include(i => i.Course).Where(i => i.Name.Contains(Name)).ToList();
+
+            return View("search",matchListModel);
+        }
+
+
+        public IActionResult Remove(int id)
+        {
+            Instructor MatchedInstructor =DB.Instructors.FirstOrDefault(i=> i.Id==id);
+            DB.Instructors.Remove(MatchedInstructor);
+            DB.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
     }
 }
--- obj/Debug/net9.0/Assignment1.AssemblyInfo.cs
+++ obj/Debug/net9.0/Assignment1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e74ba01bf1f0a0f8b5c305fdfb908f70d6060a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0270368f0e12c8dc6a5beaa26f647ab1a8d3d2f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
